10fd668b0763adc106d1962bf0ccbe4be8e28d25
likeam <92205472+likeam@users.noreply.github.com>
2022-01-18 21:42:44 +0500

    k

--- app.js
+++ app.js
@@ -160,3 +160,9 @@ btnLogin.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+btnTransfer.addEventListener('click', function(e){
+  e.preventDefault();
+  const amount =Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
+  console.log(amount, receiverAcc);
+});
